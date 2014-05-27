df859d6e8eefe9c8c11b0df9f24653969b27e4b3
Daryl Lei Chang <dlchang@corn07.stanford.edu>
2014-05-26 21:39:50 -0700

    Wrote run.sh

--- files/run.sh
+++ files/run.sh
@@ -1,8 +1,40 @@
  #!/bin/bash
 
-iterations=10
+iterations=2
 
-for i in $(seq 1 $iterations)
+for i in $(seq 0 $((iterations-1)))
 do
-	echo $iterations
+	# Clear train and test feature files
+	rm -f feats/train.lsvm
+	rm -f feats/test.lsvm
+	
+	for language in ../data/*
+	do 
+		numFiles=$(ls $language| wc -l)
+		testFileIndex=$(expr $i % $numFiles)
+		count=0
+		
+		# Run OpenSMILE and record train vs. test
+		for j in $language/*
+		do
+			if [ $count != $testFileIndex ]
+				then
+					echo "Extracting features for training example: $j"
+					# Extract the features and write to train.lsvm
+					openSMILE/SMILExtract_standalone_64bit_generic -C config/cs224s.conf -I $j -O feats/train.lsvm -noconsoleoutput
+				else
+					echo "Extracting features for test example: $j"
+					# Extract features and write to test.lsvm
+					openSMILE/SMILExtract_standalone_64bit_generic -C config/cs224s.conf -I $j -O feats/test.lsvm -noconsoleoutput
+			fi
+			# Run Python script to format last line, provide file name as argument
+			((count++))
+		done
+	done
+
+	# Run Python classifier on the train and test data
+	# Returns an accuracy, append to array
+
 done
+
+# Average accuracies and print out
