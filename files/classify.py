e2a716fdfb82b5199ce33b9e088b7ab7f6bd93c3
Daryl Lei Chang <dlchang@corn03.stanford.edu>
2014-05-27 17:21:00 -0700

    Changed array size

--- files/classify.py
+++ files/classify.py
@@ -24,7 +24,7 @@ languageLabels = np.array([int(line.strip('\n')) for line in languageLabels.read
 # Choose subset of features
 rowsToKeep = [0,1,2,3,4,5,6,7,8,9,10,11,12,15,16,31] # 0-indexed
 colsToKeep = [1,2,5,6,7,9,10,11]                    # 0-indexed
-featureSubset = np.empty([len(features), 400])
+featureSubset = np.empty([len(features), 128])
 
 for i in range(len(features)):
 	lineSubset = np.reshape(features[i], (32,12))
@@ -37,6 +37,6 @@ print featureSubset
 	
 
 # Run k-fold cross-validation on classifier
-scores = cross_validation.cross_val_score(clf, features, languageLabels, cv=k)
+scores = cross_validation.cross_val_score(clf, featureSubset, languageLabels, cv=k)
 print scores
 print scores.mean()
