c0b3bed45ec6899e4e24659da1a5d2af68812a66
Daryl Lei Chang <dlchang@corn03.stanford.edu>
2014-05-27 17:18:40 -0700

    Working on feature subset

--- files/classify.py
+++ files/classify.py
@@ -22,8 +22,19 @@ features = np.array(features)
 languageLabels = np.array([int(line.strip('\n')) for line in languageLabels.readlines()])
 
 # Choose subset of features
-rowsToKeep = [0,1,2,3,4,5,73,85,97,109,121,133,145,181,193,373]
-colsToKeep = [2,3,6,7,8,10,11,12]
+rowsToKeep = [0,1,2,3,4,5,6,7,8,9,10,11,12,15,16,31] # 0-indexed
+colsToKeep = [1,2,5,6,7,9,10,11]                    # 0-indexed
+featureSubset = np.empty([len(features), 400])
+
+for i in range(len(features)):
+	lineSubset = np.reshape(features[i], (32,12))
+	lineSubset = lineSubset[rowsToKeep]
+	lineSubset = lineSubset[:,colsToKeep]
+	lineSubset = np.reshape(lineSubset, lineSubset.size)
+	featureSubset[i] = lineSubset
+
+print featureSubset
+	
 
 # Run k-fold cross-validation on classifier
 scores = cross_validation.cross_val_score(clf, features, languageLabels, cv=k)
