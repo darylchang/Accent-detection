37212aac25eb47c1cf0ebf5f7fbe0b4d6f588660
Daryl Lei Chang <dlchang@corn12.stanford.edu>
2014-05-28 20:13:17 -0700

    Working on gender labels

--- files/classify.py
+++ files/classify.py
@@ -20,11 +20,14 @@ for line in featureLines:
 
 features = np.array(features)
 languageLabels = np.array([int(line.strip('\n')) for line in languageLabels.readlines()])
+genderLabels = np.array([line.strip('\n') for line in genderLabels.readlines()])
+print genderLabels
 
 # Choose subset of features
 rowsToKeep = [0,1,2,3,4,5,6,7,8,9,10,11,12,15,16,31] # 0-indexed
 colsToKeep = [1,2,5,6,7,9,10,11]                    # 0-indexed
-featureSubset = np.empty([len(features), 128])
+numFeatures = len(rowsToKeep) * len(colsToKeep)
+featureSubset = np.empty([len(features), numFeatures])
 
 for i in range(len(features)):
 	lineSubset = np.reshape(features[i], (32,12))
@@ -34,7 +37,9 @@ for i in range(len(features)):
 	featureSubset[i] = lineSubset
 
 print featureSubset
-	
+
+#maleIndices = [i for i in range(len))]
+#maleFeatureSubset = [for line ]
 
 # Run k-fold cross-validation on classifier
 scores = cross_validation.cross_val_score(clf, featureSubset, languageLabels, cv=k)
