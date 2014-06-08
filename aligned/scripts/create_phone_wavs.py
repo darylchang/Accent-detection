cbf59424ccb05ca934c21ad48327b2e8c82d6993
Daryl Chang <darylc123@gmail.com>
2014-06-08 00:11:30 -0700

    Retooled to run p2fa

--- aligned/scripts/create_phone_wavs.py
+++ aligned/scripts/create_phone_wavs.py
@@ -5,20 +5,19 @@ phonemes = pickle.load(open('../../metadata/phonemes.pickle','r'))
 
 for language in sorted(os.listdir('../textgrid/')):
 	for f in sorted(os.listdir('../textgrid/{}/'.format(language))):
+		print "Processing " + f
 		filePath= '../textgrid/{}/{}'.format(language,f)
 		lines = [line.strip('[\t\n]') for line in open(filePath, 'r').readlines()]
-		startCutoff = lines.index('intervals [1]:')
-		endCutoff = lines.index('item [2]:')
+		startCutoff = lines.index("\"IntervalTier\"") + 5
+		endCutoff = len(lines) - lines[::-1].index("\"IntervalTier\"") - 1
 		lines = lines[startCutoff:endCutoff]
 		
 		# Use sox to trim files into individual phones and write to wav directory
-		for i in range(len(lines)/4):
-			xmin, xmax, phone = lines[i*4+1:i*4+4]
-			xmin = re.findall('xmin = (\d*\.\d*)', xmin)[0]
-			xmax = re.findall('xmax = (\d*\.\d*)', xmax)[0]
-			phone = re.findall('text = "(.*)"', phone)[0].strip('[012]')
+		for i in range(len(lines)/3):
+			xmin, xmax, phone = lines[i*3:i*3+3]
+			phone = phone.strip('\"')
 
-			if phone in phonemes:
+			if phone in phonemes and phone not in ['sp','sil']:
 				outputPath = '../wav/{}/{}-{}-{}.wav'.format(language, f.replace('.TextGrid',''), phone, i)
 				inputPath = '../../nonaligned/wav/{}/{}'.format(language, f.replace('.TextGrid','.wav'))
 				subprocess.call(['../../sox/sox', inputPath, outputPath, 'trim', xmin, '={}'.format(xmax)])		
